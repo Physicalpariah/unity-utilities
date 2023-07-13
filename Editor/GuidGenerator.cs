102e081d2c5692686d0d7706a8c06eba47599055
Matt <purchasm@gmail.com>
2023-07-13 16:37:17 +1000

    int guid

--- Editor/GuidGenerator.cs
+++ Editor/GuidGenerator.cs
@@ -6,12 +6,26 @@ using UnityEngine;
 
 public class GuidGenerator {
 	[MenuItem("Anchorite/utils/Generate Guid %g")]
-	public static void CaptureScreen() {
+	public static void StringGuid() {
 		string guid = Guid.NewGuid().ToString();
 		TextEditor te = new TextEditor();
 		te.text = guid;
 		te.SelectAll();
 		te.Copy();
+
+		LogUtils.Log("GUID added to clipboard");
+		LogUtils.Log(guid);
+	}
+
+
+	[MenuItem("Anchorite/utils/Generate Guid Integer %f")]
+	public static void IntGuid() {
+		int guid = Guid.NewGuid().GetHashCode();
+		TextEditor te = new TextEditor();
+		te.text = guid.ToString();
+		te.SelectAll();
+		te.Copy();
+		LogUtils.Log(guid);
 
 		LogUtils.Log("GUID added to clipboard");
 		LogUtils.Log(guid);
